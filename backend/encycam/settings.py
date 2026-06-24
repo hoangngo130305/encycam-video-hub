c600b033f7ab1f9e56d4e0c2ae25d02a415060d5
Adidas <hoangngoAZ8799@gmail.com>
2026-06-24 18:39:05 +0700

    update

--- backend/encycam/settings.py
+++ backend/encycam/settings.py
@@ -203,8 +203,7 @@ DATABASES = {
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
-        'CONN_MAX_AGE': 300,
-        'CONN_HEALTH_CHECKS': True,
+        'CONN_MAX_AGE': 0,
     }
 }
 
